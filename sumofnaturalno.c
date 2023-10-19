82b04767dec75e8aa6d0cdfcabfd6f1bb106e8f3
Aadityapratap97 <146857549+Aadityapratap97@users.noreply.github.com>
2023-10-20 00:02:17 +0530

    Add files via upload

---
+++ sumofnaturalno.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,sum;
+    printf("enter the i value to print natural number",0);
+    {
+        for(j=1;i<=i;j++)
+        sum=sum+j;
+        printf("the sum of the natural numbers:%d",sum);
+    }
+    return 0;
+}
